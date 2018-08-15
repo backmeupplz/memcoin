4b380b67d69353311ac283dcd2a7d821113c0d28
Max Frolov <maffazi@gmail.com>
2018-08-15 15:05:12 +0300

    Change spaces->tabs, change namingsofusers, add amount check

--- src/commands/toxics.ts
+++ src/commands/toxics.ts
@@ -5,36 +5,39 @@ import { isReply } from './middleware'
 import { transfer } from './transfer'
 
 function isToxicMinisters(user: IUser) {
-    // Check if ministers
-    return [56778694].indexOf(user.chatId) > -1
+	// Check if ministers
+	return [56778694].indexOf(user.chatId) > -1
   }
 
 // Ministry of toxic commands
 export function setupToxicCommand(bot: Telegraf<ContextMessageUpdate>) {
-    bot.hears('prepareforkick', isReply, isToxicMinisters, checkAssets)
+	bot.hears('prepareforkick', isReply, isToxicMinisters, checkAssets)
   }
 
 async function checkAssets(ctx: ContextMessageUpdate) {
   // Get defendant
-  let sender = await getUser(ctx.message.reply_to_message.from.id)
+  const defendant = await getUser(ctx.message.reply_to_message.from.id)
   // Get defendant Info
   const defendantInfo = await getUserInfo(ctx.telegram, sender)
   // Get amount
   const amount = defendantInfo.balance
-  // Get receiver
-  const receiver = await getUser(ctx.from.id)
+  if (!amount || amount <=0) return
+  // Get bailiff
+	const bailiff = await getUser(ctx.from.id)
+	// Get bailiff Info
+	const bailiffInfo = await getUserInfo(ctx.telegram, bailiff)
   try {
-    // Transfer coins
-    await transfer(sender, receiver, amount)
-    // Reply
-    const text = `Приставы посетили *${defendantInfo.name}* и избяли у Гаражанина *${amount}* Мемкоинов. Теперб у нихм ${defendantInfo.balance} Мемкоинов.`
-    await ctx.replyWithMarkdown(text, {
-      reply_to_message_id: ctx.message.message_id,
-    })
+		// Transfer coins
+		await transfer(defendant, bailiff, amount)
+		// Reply
+		const text = `Приставм *${bailiffInfo.name}* посетилм *${defendantInfo.name}* и избял у Гаражанина *${amount}* Мемкоинов. Теперб у нихм ${defendantInfo.balance} Мемкоинов.`
+		await ctx.replyWithMarkdown(text, {
+	  	reply_to_message_id: ctx.message.message_id,
+		})
   } catch (err) {
-    await ctx.replyWithMarkdown(err.message, {
-      reply_to_message_id: ctx.message.message_id,
-    })
-    return
+		await ctx.replyWithMarkdown(err.message, {
+	  	reply_to_message_id: ctx.message.message_id,
+		})
+		return
   }
 }
