baae236550deba7634bd41fd35ae6c9e62888c27
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-04-08 11:23:05 -0700

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -5,7 +5,7 @@ import { Telegraf, ContextMessageUpdate } from 'telegraf'
 export function setupHelp(bot: Telegraf<ContextMessageUpdate>) {
   bot.command(['start', 'help'], (ctx) => {
     // Prepare text
-    const text = `Дароуствуйте! Это бот Мемкономики.\n\n/help — это сообщение\n/balance — ваш баланс в Мемкоинах\n/leaderboard — топ Мемолиархов\n\nЕсли хотите перевести кому-нибудь Мемкоинов или выпустить новые (и у вас есть на это права), просто ответьте на сообщение человеку плюсом, сколько будет плюсов, столько и переведете.\n\n*Боже, храни Мемарха!*`
+    const text = `Здравствуйте! Это бот, наполненный любовью.\n\n/help — это сообщение\n/balance — сколько у вас любви\n/leaderboard — топ любимых\n\nЕсли вы хотите выразить кому-нибудь свою любовь, просто ответьте на сообщение человеку плюсом или сердечком, сколько будет плюсов или сердечков, столько любви и придет получателю.\n\n*All you need is love!*`
     // Reply
     ctx.replyWithMarkdown(text)
   })
