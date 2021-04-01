06322f5ba351e98191f06919a3de77b837be9c5c
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-04-01 09:33:30 -0700

    Update transfer.ts

--- src/helpers/transfer.ts
+++ src/helpers/transfer.ts
@@ -48,7 +48,7 @@ async function mint(user: IUser, amount: number) {
   return user.save()
 }
 
-const stickerFileIds = { 'AgADAgADf3BGHA': 20 }
+const stickerFileIds = { 'AgADAgADf3BGHA': 20 } as {[index: string]: number }
 async function checkTransfer(ctx: ContextMessageUpdate) {
   // Check if sticker
   let amount = 0
@@ -65,7 +65,7 @@ async function checkTransfer(ctx: ContextMessageUpdate) {
       allowed = true
     }
     if (allowed) {
-      amount = isSticker ? stickerFileIds[(ctx.message.sticker as any).file_unique_id] : 1
+      amount = isSticker ? stickerFileIds[(ctx.message.sticker as any).file_unique_id as string] : 1
     } else {
       return
     }
